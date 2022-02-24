f73d020eb16041b6f9cfb63513091e374636ea9a
Nguimeya Bill-gates <bnguimeya007@gmail.com>
2022-02-24 05:15:07 +0100

    hackerRank link

--- components/Presentation.js
+++ components/Presentation.js
@@ -6,6 +6,7 @@ import {
   FaGithub,
   FaTwitter,
   FaInstagram,
+  FaHackerrank,
 } from "react-icons/fa";
 
 function Presentation() {
@@ -13,14 +14,14 @@ function Presentation() {
     <section className="mx-4 flex flex-col sm:mx-0 md:flex-row xl:justify-around">
       <div className="presentation flex flex-col justify-center text-center md:text-left">
         <p>Hi, I am</p>
-        <h1 className="break-words font-body text-hd1 leading-snug bg-clip-text bg-gradient-to-r from-secondary to-primary dark:from-rose-500 dark:to-red-900 text-transparent md:max-w-md msm:text-5xl">
+        <h1 className="font-body text-hd1 from-secondary to-primary dark:from-rose-500 msm:text-5xl break-words bg-gradient-to-r bg-clip-text leading-snug text-transparent dark:to-red-900 md:max-w-md">
           Nguimeya Bill-gates
         </h1>
         <p className="mt-6 mb-10 md:max-w-md">
           front end developer and graphic designer from{" "}
-          <span className="text-secondary dark:text-rose-500">Yaounde</span>, Cameroon. I
-          specialize Web development and occasionally designing digital
-          experiences.
+          <span className="text-secondary dark:text-rose-500">Yaounde</span>,
+          Cameroon. I specialize Web development and occasionally designing
+          digital experiences.
         </p>
         <div className="mb-6">
           <Link href="https://resume.io/r/7bUEJMzgT" target="_blank">
@@ -29,10 +30,26 @@ function Presentation() {
         </div>
         <div className="mt-5 flex justify-center md:justify-start">
           <a
+            href="https://www.hackerrank.com/no_bill"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon text-primary hover:text-secondary dark:hover:text-rose-500 mr-2 text-2xl"
+          >
+            <FaHackerrank />
+          </a>
+          <a
+            href="https://github.com/nobill1"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="icon text-primary hover:text-secondary dark:hover:text-rose-500 mr-2 text-2xl"
+          >
+            <FaGithub />
+          </a>
+          <a
             href="https://www.linkedin.com/in/nguimeya-bill-gates-9b614b146"
             target="_blank"
             rel="noopener noreferrer"
-            className="icon mr-2 text-2xl text-primary hover:text-secondary dark:hover:text-rose-500"
+            className="icon text-primary hover:text-secondary dark:hover:text-rose-500 mr-2 text-2xl"
           >
             <FaLinkedinIn />
           </a>
@@ -40,37 +57,21 @@ function Presentation() {
             href="https://codepen.io/Bill1"
             target="_blank"
             rel="noopener noreferrer"
-            className="icon mr-2 text-2xl text-primary hover:text-secondary dark:hover:text-rose-500"
+            className="icon text-primary hover:text-secondary dark:hover:text-rose-500 mr-2 text-2xl"
           >
             <FaCodepen />
-          </a>
-          <a
-            href="https://github.com/nobill1"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon mr-2 text-2xl text-primary hover:text-secondary dark:hover:text-rose-500"
-          >
-            <FaGithub />
-          </a>
-          <a
-            href="https://twitter.com/go_bill_go"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="icon mr-2 text-2xl text-primary hover:text-secondary dark:hover:text-rose-500"
-          >
-            <FaTwitter />
           </a>
           <a
             href="https://www.instagram.com/go_bill_go/"
             target="_blank"
             rel="noopener noreferrer"
-            className="icon text-2xl text-primary hover:text-secondary dark:hover:text-rose-500"
+            className="icon text-primary hover:text-secondary dark:hover:text-rose-500 text-2xl"
           >
             <FaInstagram />
           </a>
         </div>
       </div>
-      <div className="flex w-60 flex-1 items-center justify-center xl:w-80 mmd:hidden">
+      <div className="mmd:hidden flex w-60 flex-1 items-center justify-center xl:w-80">
         <Illustration className="w-60 xl:w-80" />
       </div>
     </section>
