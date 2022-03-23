44f931b2c34489a52f6b428251f2ad03f23c7164
Nguimeya Bill-gates <bnguimeya007@gmail.com>
2022-03-23 18:37:18 +0100

    Fix logo size loader

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ function Index() {
   useEffect(() => {
     id.current = window.setInterval(() => {
       setTimer((timer) => timer - 1);
-    }, 2000);
+    }, 1000);
   }, []);
 
   useEffect(() => {
@@ -37,7 +37,7 @@ function Index() {
       <Head>Portfolio / Nguimeya Bill-gates</Head>
       {preloader ? (
         <div className="loader-wrapper absolute">
-          <Logo className="logoSplash h-64 w-auto" />
+          <Logo className="logoSplash w-auto" />
         </div>
       ) : (
         <>
