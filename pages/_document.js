a31c32652b3d08e95b71d7845e67f3d4e08942ba
Nguimeya Bill-gates <bnguimeya007@gmail.com>
2024-01-18 20:53:47 +0100

    seo management

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,30 @@ class MyDocument extends Document {
     return (
       <Html lang='en-US' className="scroll-smooth">
         <Head>
-          <meta name="description" content="Software engineer | Fullstack developer" />
+          <meta name="description" content="Experienced full-stack developer skilled in multiple programming languages. Offering expertise in front-end and back-end development, database management, and web application design."/>
+          <meta name="keywords" content="full-stack developer, web developer, front-end developer, back-end developer, programming languages, JavaScript, HTML, CSS, Python, SQL, MongoDB, MySQL, web application development"/>
+          <meta name="author" content="Bill Nguimeya"/>
+
+          <meta property="og:title" content="Bill Nguimeya - Full-Stack Developer"/>
+          <meta property="og:description" content="Experienced full-stack developer skilled in multiple programming languages. Offering expertise in front-end and back-end development, database management, and web application design."/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://billdev.netlify.app/"/>
+          <meta property="og:image" content="/logo.svg"/>
+
+       
+          <meta property="og:title" content="Bill Nguimeya - Full-Stack Developer"/>
+          <meta property="og:description" content="Experienced full-stack developer skilled in multiple programming languages. Offering expertise in front-end and back-end development, database management, and web application design."/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://billdev.netlify.app/"/>
+          <meta property="og:image" content="/logo.svg"/>
+          <meta property="og:site_name" content="Bill Nguimeya - Full-Stack Developer"/>
+          <meta property="profile:first_name" content="Bill"/>
+          <meta property="profile:last_name" content="Nguimeya"/>
+          <meta property="profile:industry" content="Information Technology and Services"/>
+          <meta property="profile:position:web-developer" content="Full-Stack Developer"/>
+
+
+
           <link rel="icon" href="/favicon.png" />
           <link
             href="https://api.fontshare.com/css?f[]=nippo@500&display=swap"
