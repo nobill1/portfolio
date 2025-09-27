d835af74d2c9dfca8643704dea95e47c84cca0ae
Bill Nguimeya <bnguimeya007@gmail.com>
2025-09-27 16:51:17 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
 module.exports = {
   reactStrictMode: true,
-  output: 'export',
+  // output: 'export',
   // Enable trailing slashes for better compatibility
   trailingSlash: true,
   // Disable powered-by header for security
   poweredByHeader: false,
   images: {
-    domains: ["res.cloudinary.com"]
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],    
   }
 }
